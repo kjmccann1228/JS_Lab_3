ceadb2f6cc2be310a9fa318e30e3347f479f016c
Kurt McCann <kurtmccann@MacBook-Pro-20.local>
2016-10-09 10:04:46 -0400

    Part 1: Added add, lookup, remove, list names and list number

--- js/script.js
+++ js/script.js
@@ -1,28 +1,67 @@
 " use strict ";
+var display = document.getElementById("display");
 
-function PhoneBook(){
+var pb = new PhoneBook();
+var counter = 0;
+function PhoneBook()
+{
+  this.count = 0;
+  this.phoneBook = [];
 
 };
 
 function listAllNames() {
   // code to list all names
+  var str = "";
+  for(var i=0 ; i < pb.phoneBook.length ; i++)
+  {
+    str += pb.phoneBook[i].name + "<br />";
+  }
+  display.innerHTML = str;
 };
 
 function listAllNumbers() {
   // code to list all numbers
 };
 
+function Contact(name, number)
+{
+  this.name = name;
+  this.number = number;
+}
+
 function showAdd() {
+
   var name = prompt("Enter full name");
   var number = prompt("Enter phone number");
+  pb.phoneBook.push(new Contact(name, number));
+  console.log(pb.phoneBook[counter].name + " " + pb.phoneBook[counter].number);
+  counter++;
 };
 
 function showRemove() {
   var remove = prompt("Enter name to remove");
+  for(var i = 0 ; i < pb.phoneBook.length ; i++)
+  {
+    if(pb.phoneBook[i].name == remove)
+    {
+      pb.phoneBook.splice(i,1);
+    }
+  }
+
 };
 
 function showLookup() {
-  var lookup = prompt("Enter number to lookup");
+  var lookup = prompt("Enter name to lookup");
+  var num = "";
+  for(var i = 0 ; i < pb.phoneBook.length ; i++)
+  {
+    if(pb.phoneBook[i].name == lookup)
+    {
+      num += pb.phoneBook[i].number;
+    }
+  }
+  display.innerHTML = num;
 };
 
 var display = document.getElementById("display");
